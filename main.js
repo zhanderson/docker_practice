6138a7bb554e5de8aaf1ee368dcb0923cad3b510
zhanderson <unicorn731@163.com>
2018-11-07 22:05:17 +0800

    delete one line

--- main.js
+++ main.js
@@ -2,5 +2,4 @@ console.log('hello git');
 console.table({a: 'a', b: 'b'});
 console.log('error console');
 console.log('new branch');
-console.log('trigger jenkins');
 console.log('trigger jenkins again!');
